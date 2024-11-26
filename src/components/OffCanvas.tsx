b4a6b5f5ba089c98bc4d29f53f7f0ec6070d2f6b
np03a170120 <czon.herald75@gmail.com>
2024-11-26 13:41:25 +0545

    feat: ui update cart

--- src/components/OffCanvas.tsx
+++ src/components/OffCanvas.tsx
@@ -1,3 +1,4 @@
+import { X } from "@phosphor-icons/react";
 import { useCartContext } from "../context/CartContext";
 import { useOffCanvas } from "../context/OffCanvasContext";
 
@@ -10,9 +11,20 @@ const OffCanvas = () => {
       <>
         {cartItems.map((item) => (
           <>
-            <p>{item.title}</p>
-            <p>{item.price}</p>
-            <p>{item.quantity}</p>
+            <div className="flex items-center justify-between mb-4">
+              <div className="flex gap-6">
+                <img
+                  className="w-20 rounded-lg h-20 bg-gray-50"
+                  src={item.images}
+                  alt=""
+                />
+                <div>
+                  <p>{item.title}</p>
+                  <p>${item.price}</p>
+                </div>
+              </div>
+              <p>{item.quantity}</p>
+            </div>
           </>
         ))}
       </>
@@ -21,13 +33,24 @@ const OffCanvas = () => {
 
   return (
     <div
-      className={`h-dvh delay-100 transition-all fixed right-[-400px] top-0 w-[24rem] bg-gray-100 overflow-scroll ${isOnoffcanvas ? " !right-0 transition-all z-10 " : "z-[-1]"}`}
+      className={`h-dvh delay-100 p-4 transition-all fixed right-[-600px] top-0 w-[32rem] bg-gray-900 text-white overflow-scroll ${isOnoffcanvas ? " !right-0 transition-all z-10 " : "z-[-1]"}`}
     >
-      <button onClick={() => toggleCanvas()}>close btn</button>
-      <button onClick={() => emptyCart()}>Empty cart</button>
-      <h1>offcanvas</h1>
+      <div
+        onClick={() => toggleCanvas()}
+        className=" cursor-pointer flex justify-end  w-full text-end"
+      >
+        <X size={32} />
+      </div>
+
+      <h1 className="text-lg text-center mb-6">CART</h1>
       <AvailableCartItems />
-      <h1>Total Items: {cartQuantity} </h1>
+      <h1 className="text-lg mb-6">Total Items: {cartQuantity} </h1>
+      <button
+        className="bg-red-600 p-3 py-2 rounded-lg"
+        onClick={() => emptyCart()}
+      >
+        Clear cart
+      </button>
     </div>
   );
 };
